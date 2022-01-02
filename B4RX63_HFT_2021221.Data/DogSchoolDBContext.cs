5a4c3a0908412dc4c64d6bb301108f4237c4fbfa
Babka Eszter <babkaeszter@stud.uni-obuda.hu>
2022-01-02 15:21:47 +0100

    refactroing

--- B4RX63_HFT_2021221.Data/DogSchoolDBContext.cs
+++ B4RX63_HFT_2021221.Data/DogSchoolDBContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using B4RX63_HFT_2021221.Models;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,9 +10,9 @@ namespace B4RX63_HFT_2021221.Data
 {
     public class DogSchoolDBContext : DbContext 
     {
-        public virtual DbSet<Models.Dog> Dogs { get; set; } 
-        public virtual DbSet<Models.Owner> Owners { get; set; }
-        public virtual DbSet<Models.Course> Courses { get; set; }
+        public virtual DbSet< Dog> Dogs { get; set; } 
+        public virtual DbSet< Owner> Owners { get; set; }
+        public virtual DbSet< Course> Courses { get; set; }
 
         public DogSchoolDBContext()
         {
@@ -27,7 +28,7 @@ namespace B4RX63_HFT_2021221.Data
         }
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            modelBuilder.Entity<Models.Dog>(entity =>
+            modelBuilder.Entity<Dog>(entity =>
             {
                 entity
                 .HasOne(dog => dog.Owner)
@@ -36,7 +37,7 @@ namespace B4RX63_HFT_2021221.Data
                 .OnDelete(DeleteBehavior.Restrict);
             }
             );
-            modelBuilder.Entity<Models.Owner>(entity =>
+            modelBuilder.Entity< Owner>(entity =>
             {
                 entity.HasOne(owner => owner.Course)
                 .WithMany(course => course.ParticipantOwners)
@@ -44,7 +45,7 @@ namespace B4RX63_HFT_2021221.Data
                 .OnDelete(DeleteBehavior.Restrict);
             }
             );
-            modelBuilder.Entity<Models.Dog>(entity =>
+            modelBuilder.Entity< Dog>(entity =>
             {
                 entity.HasOne(dog => dog.Course)
                 .WithMany(course => course.ParticipantDogs)
@@ -53,44 +54,44 @@ namespace B4RX63_HFT_2021221.Data
             }
             );
             //Courses
-            Models.Course alap = new Models.Course { Id = 1, Name = "Alapozó tanfolyam", Organizer = "Fővárosi Kutyások Egyesülete", Trainer = "Szűcs Zoltán" };
-            Models.Course halado = new Models.Course { Id = 2, Name = "Haladó tanfolyam", Organizer = "Kutyás Élet Kutyaiskola", Trainer = "Lengyel Pálné" };
-            Models.Course agility = new Models.Course { Id = 3, Name = "Agility tanfolyam", Organizer = "Sportkutyások Egyesülete", Trainer = "Kiss Bettina" };
-            Models.Course ovt = new Models.Course { Id = 4, Name = "őrző-védő tanfolyam", Organizer = "Heroic Dogs Sportegyesület", Trainer = "Soós Miklós" };
+            Course alap = new Course { Id = 1, Name = "Alapozó tanfolyam", Organizer = "Fővárosi Kutyások Egyesülete", Trainer = "Szűcs Zoltán" };
+            Course halado = new Course { Id = 2, Name = "Haladó tanfolyam", Organizer = "Kutyás Élet Kutyaiskola", Trainer = "Lengyel Pálné" };
+            Course agility = new Course { Id = 3, Name = "Agility tanfolyam", Organizer = "Sportkutyások Egyesülete", Trainer = "Kiss Bettina" };
+            Course ovt = new Course { Id = 4, Name = "Őrző-védő tanfolyam", Organizer = "Heroic Dogs Sportegyesület", Trainer = "Soós Miklós" };
             //Owners
-            Models.Owner tamas = new Models.Owner() { Id = 1, Name = "Kiss Tamás", Sex = Models.Gender.male, Age = 34, CourseId = alap.Id };
-            Models.Owner eniko = new Models.Owner() { Id = 2, Name = "Szabó Enikő", Sex = Models.Gender.female, Age = 22, CourseId = alap.Id };
-            Models.Owner istvan = new Models.Owner() { Id = 3, Name = "Varga István", Sex = Models.Gender.male, Age = 29, CourseId = alap.Id };
-            Models.Owner dora = new Models.Owner() { Id = 4, Name = "Nagy Dóra", Sex = Models.Gender.female, Age = 43, CourseId = alap.Id };
-            Models.Owner anna = new Models.Owner() { Id = 5, Name = "Kovács Anna", Sex = Models.Gender.female, Age = 19, CourseId = halado.Id };
-            Models.Owner erno = new Models.Owner() { Id = 6, Name = "Magyar Ernő", Sex = Models.Gender.male, Age = 26, CourseId = halado.Id };
-            Models.Owner bela = new Models.Owner() { Id = 7, Name = "Váci Béla", Sex = Models.Gender.male, Age = 32, CourseId = halado.Id };
-            Models.Owner miklos = new Models.Owner() { Id = 8, Name = "Varró Miklós", Sex = Models.Gender.male, Age = 27, CourseId = agility.Id };
-            Models.Owner csaba = new Models.Owner() { Id = 9, Name = "Tóth Csaba", Sex = Models.Gender.male, Age = 23, CourseId = agility.Id };
-            Models.Owner kitti = new Models.Owner() { Id = 10, Name = "Csorba Kitti", Sex = Models.Gender.female, Age = 30, CourseId = agility.Id };
-            Models.Owner zoltan = new Models.Owner() { Id = 11, Name = "Nemes Zoltán", Sex = Models.Gender.male, Age = 21, CourseId = ovt.Id };
-            Models.Owner agnes = new Models.Owner() { Id = 12, Name = "Kókai Ágnes", Sex = Models.Gender.female, Age = 27, CourseId = ovt.Id };
-            Models.Owner adam = new Models.Owner() { Id = 13, Name = "Komor Adam", Sex = Models.Gender.male, Age = 35, CourseId = ovt.Id };
-            Models.Owner akos = new Models.Owner() { Id = 14, Name = "Horváth Ákos", Sex = Models.Gender.male, Age = 24, CourseId = ovt.Id };
+             Owner tamas = new  Owner() { Id = 1, Name = "Kiss Tamás", Sex =  Gender.male, Age = 34, CourseId = alap.Id };
+             Owner eniko = new  Owner() { Id = 2, Name = "Szabó Enikő", Sex =  Gender.female, Age = 22, CourseId = alap.Id };
+             Owner istvan = new  Owner() { Id = 3, Name = "Varga István", Sex =  Gender.male, Age = 29, CourseId = alap.Id };
+             Owner dora = new  Owner() { Id = 4, Name = "Nagy Dóra", Sex =  Gender.female, Age = 43, CourseId = alap.Id };
+             Owner anna = new  Owner() { Id = 5, Name = "Kovács Anna", Sex =  Gender.female, Age = 19, CourseId = halado.Id };
+             Owner erno = new  Owner() { Id = 6, Name = "Magyar Ernő", Sex =  Gender.male, Age = 26, CourseId = halado.Id };
+             Owner bela = new  Owner() { Id = 7, Name = "Váci Béla", Sex =  Gender.male, Age = 32, CourseId = halado.Id };
+             Owner miklos = new  Owner() { Id = 8, Name = "Varró Miklós", Sex =  Gender.male, Age = 27, CourseId = agility.Id };
+             Owner csaba = new  Owner() { Id = 9, Name = "Tóth Csaba", Sex =  Gender.male, Age = 23, CourseId = agility.Id };
+             Owner kitti = new  Owner() { Id = 10, Name = "Csorba Kitti", Sex =  Gender.female, Age = 30, CourseId = agility.Id };
+             Owner zoltan = new  Owner() { Id = 11, Name = "Nemes Zoltán", Sex =  Gender.male, Age = 21, CourseId = ovt.Id };
+             Owner agnes = new  Owner() { Id = 12, Name = "Kókai Ágnes", Sex =  Gender.female, Age = 27, CourseId = ovt.Id };
+             Owner adam = new  Owner() { Id = 13, Name = "Komor Adam", Sex =  Gender.male, Age = 35, CourseId = ovt.Id };
+             Owner akos = new  Owner() { Id = 14, Name = "Horváth Ákos", Sex =  Gender.male, Age = 24, CourseId = ovt.Id };
             //Dogs
-            Models.Dog buksi = new Models.Dog() { Id = 1, Name = "Buksi", Breed = "Puli", Sex = Models.Gender.male, Castrated = true, Weight = 13.4, Height = 42, OwnerId = tamas.Id, CourseId = alap.Id };
-            Models.Dog poppy = new Models.Dog() { Id = 2, Name = "Poppy", Breed = "Dalmata", Sex = Models.Gender.female, Castrated = false, Weight = 20.3, Height = 56, OwnerId = eniko.Id, CourseId = alap.Id };
-            Models.Dog athos = new Models.Dog() { Id = 3, Name = "Athos", Breed = "Német juhászkutya", Sex = Models.Gender.male, Castrated = false, Weight = 39, Height = 62.5, OwnerId = istvan.Id, CourseId = alap.Id };
-            Models.Dog picur = new Models.Dog() { Id = 4, Name = "Picúr", Breed = "Yorkshire terrier", Sex = Models.Gender.female, Castrated = true, Weight = 2.8, Height = 20, OwnerId = dora.Id, CourseId = alap.Id };
-            Models.Dog baba = new Models.Dog() { Id = 5, Name = "Baba", Breed = "Border collie", Sex = Models.Gender.female, Castrated = false, Weight = 16.7, Height = 48, OwnerId = anna.Id, CourseId = halado.Id };
-            Models.Dog rex = new Models.Dog() { Id = 6, Name = "Rex", Breed = "Német juhászkutya", Sex = Models.Gender.male, Castrated = true, Weight = 37.8, Height = 61.3, OwnerId = erno.Id, CourseId = halado.Id };
-            Models.Dog ludwig = new Models.Dog() { Id = 7, Name = "Ludwig", Breed = "Bichon Bolognese", Sex = Models.Gender.male, Castrated = true, Weight = 3, Height = 28, OwnerId = bela.Id, CourseId = halado.Id };
-            Models.Dog ariel = new Models.Dog() { Id = 8, Name = "Ariel", Breed = "Border collie", Sex = Models.Gender.female, Castrated = false, Weight = 17.3, Height = 50.5, OwnerId = miklos.Id, CourseId = agility.Id };
-            Models.Dog barna = new Models.Dog() { Id = 9, Name = "Barna", Breed = "Mudi", Sex = Models.Gender.male, Castrated = true, Weight = 12.6, Height = 45.6, OwnerId = csaba.Id, CourseId = agility.Id };
-            Models.Dog cuki = new Models.Dog() { Id = 10, Name = "Cuki", Breed = "Ausztrál juhászkutya", Sex = Models.Gender.female, Castrated = true, Weight = 23.4, Height = 49.7, OwnerId = kitti.Id, CourseId = agility.Id };
-            Models.Dog xanthos = new Models.Dog() { Id = 11, Name = "Xanthos", Breed = "Kaukázusi juhászkutya", Sex = Models.Gender.male, Castrated = false, Weight = 63.1, Height = 73.4, OwnerId = zoltan.Id, CourseId = ovt.Id };
-            Models.Dog potyi = new Models.Dog() { Id = 12, Name = "Pötyi", Breed = "Kuvasz", Sex = Models.Gender.female, Castrated = true, Weight = 38.5, Height = 67, OwnerId = agnes.Id, CourseId = ovt.Id };
-            Models.Dog ravasz = new Models.Dog() { Id = 13, Name = "Ravasz", Breed = "Óriás schnauzer", Sex = Models.Gender.male, Castrated = false, Weight = 45.3, Height = 66.5, OwnerId = adam.Id, CourseId = ovt.Id };
-            Models.Dog ace = new Models.Dog() { Id = 14, Name = "Ace", Breed = "Belga juhászkutya", Sex = Models.Gender.male, Castrated = true, Weight = 27, Height = 61.4, OwnerId = akos.Id, CourseId = ovt.Id };
+             Dog buksi = new  Dog() { Id = 1, Name = "Buksi", Breed = "Puli", Sex =  Gender.male, Castrated = true, Weight = 13.4, Height = 42, OwnerId = tamas.Id, CourseId = alap.Id };
+             Dog poppy = new  Dog() { Id = 2, Name = "Poppy", Breed = "Dalmata", Sex =  Gender.female, Castrated = false, Weight = 20.3, Height = 56, OwnerId = eniko.Id, CourseId = alap.Id };
+             Dog athos = new  Dog() { Id = 3, Name = "Athos", Breed = "Német juhászkutya", Sex =  Gender.male, Castrated = false, Weight = 39, Height = 62.5, OwnerId = istvan.Id, CourseId = alap.Id };
+             Dog picur = new  Dog() { Id = 4, Name = "Picúr", Breed = "Yorkshire terrier", Sex =  Gender.female, Castrated = true, Weight = 2.8, Height = 20, OwnerId = dora.Id, CourseId = alap.Id };
+             Dog baba = new  Dog() { Id = 5, Name = "Baba", Breed = "Border collie", Sex =  Gender.female, Castrated = false, Weight = 16.7, Height = 48, OwnerId = anna.Id, CourseId = halado.Id };
+             Dog rex = new  Dog() { Id = 6, Name = "Rex", Breed = "Német juhászkutya", Sex =  Gender.male, Castrated = true, Weight = 37.8, Height = 61.3, OwnerId = erno.Id, CourseId = halado.Id };
+             Dog ludwig = new  Dog() { Id = 7, Name = "Ludwig", Breed = "Bichon Bolognese", Sex =  Gender.male, Castrated = true, Weight = 3, Height = 28, OwnerId = bela.Id, CourseId = halado.Id };
+             Dog ariel = new  Dog() { Id = 8, Name = "Ariel", Breed = "Border collie", Sex =  Gender.female, Castrated = false, Weight = 17.3, Height = 50.5, OwnerId = miklos.Id, CourseId = agility.Id };
+             Dog barna = new  Dog() { Id = 9, Name = "Barna", Breed = "Mudi", Sex =  Gender.male, Castrated = true, Weight = 12.6, Height = 45.6, OwnerId = csaba.Id, CourseId = agility.Id };
+             Dog cuki = new  Dog() { Id = 10, Name = "Cuki", Breed = "Ausztrál juhászkutya", Sex =  Gender.female, Castrated = true, Weight = 23.4, Height = 49.7, OwnerId = kitti.Id, CourseId = agility.Id };
+             Dog xanthos = new  Dog() { Id = 11, Name = "Xanthos", Breed = "Kaukázusi juhászkutya", Sex =  Gender.male, Castrated = false, Weight = 63.1, Height = 73.4, OwnerId = zoltan.Id, CourseId = ovt.Id };
+             Dog potyi = new  Dog() { Id = 12, Name = "Pötyi", Breed = "Kuvasz", Sex =  Gender.female, Castrated = true, Weight = 38.5, Height = 67, OwnerId = agnes.Id, CourseId = ovt.Id };
+             Dog ravasz = new  Dog() { Id = 13, Name = "Ravasz", Breed = "Óriás schnauzer", Sex =  Gender.male, Castrated = false, Weight = 45.3, Height = 66.5, OwnerId = adam.Id, CourseId = ovt.Id };
+             Dog ace = new  Dog() { Id = 14, Name = "Ace", Breed = "Belga juhászkutya", Sex =  Gender.male, Castrated = true, Weight = 27, Height = 61.4, OwnerId = akos.Id, CourseId = ovt.Id };
 
-            modelBuilder.Entity<Models.Owner>().HasData(tamas, eniko, istvan, dora, anna, erno, bela, miklos, csaba, kitti, zoltan, agnes, adam, akos);
-            modelBuilder.Entity<Models.Dog>().HasData(buksi, poppy, athos, picur, baba, rex, ludwig, ariel, barna, cuki, xanthos, potyi, ravasz, ace);
-            modelBuilder.Entity<Models.Course>().HasData(alap, halado, agility, ovt);
+            modelBuilder.Entity< Owner>().HasData(tamas, eniko, istvan, dora, anna, erno, bela, miklos, csaba, kitti, zoltan, agnes, adam, akos);
+            modelBuilder.Entity< Dog>().HasData(buksi, poppy, athos, picur, baba, rex, ludwig, ariel, barna, cuki, xanthos, potyi, ravasz, ace);
+            modelBuilder.Entity< Course>().HasData(alap, halado, agility, ovt);
 
         }
     }
