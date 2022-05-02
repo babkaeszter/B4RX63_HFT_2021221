7cd7478dcdb718805e5efc9b1ac26ba524d961ec
Babka Eszter <babkaeszter@stud.uni-obuda.hu>
2022-05-02 20:30:23 +0200

    remove methods

--- B4RX63_HFT_2021221.JSClient/wwwroot/script.js
+++ B4RX63_HFT_2021221.JSClient/wwwroot/script.js
@@ -35,25 +35,28 @@ async function getCourses() {
 
 
 function displayDogs() {
+    //document.getElementById('dogs').innerHTML = ""
     dogs.forEach(d => {
         if (d.sex == 1) { gender = "Szuka" } else { gender = "Kan" };
         if (d.castrated == 1) { castrated = "Igen" } else { castrated = "Nem" };
         document.getElementById('dogs').innerHTML +=
-            "<tr><td>" + d.id + "</td><td>" + d.name + "</td><td>" + d.breed + "</td><td>" + gender + "</td><td>" + castrated + "</td><td>" + d.weight + "</td><td>" + d.height + "</td><td>" + d.ownerId + "</td > <td>" + d.courseId + "</td></tr>";
+            "<tr><td>" + d.id + "</td><td>" + d.name + "</td><td>" + d.breed + "</td><td>" + gender + "</td><td>" + castrated + "</td><td>" + d.weight + "</td><td>" + d.height + "</td><td>" + d.ownerId + "</td > <td>" + d.courseId + "</td><td><button onclick='removeDog()'>Törlés</button></td></tr>";
     });
 }
 function displayOwners() {
+    //document.getElementById('dogs').innerHTML = ""
     owners.forEach(o => {
         if (o.sex == 1) { gender = "Nõ" } else { gender = "Férfi" };
         document.getElementById('owners').innerHTML +=
-            "<tr><td>" + o.id + "</td><td>" + o.name + "</td><td>" + o.age + "</td><td>" + gender + "</td><td>"+ o.courseId + "</td></tr>";
+            "<tr><td>" + o.id + "</td><td>" + o.name + "</td><td>" + o.age + "</td><td>" + gender + "</td><td>" + o.courseId + "</td><td><button onclick='removeOwner()'>Törlés</button></td></tr>";
     });
 
 }
 function displayCourses() {
+    //document.getElementById('dogs').innerHTML =""
     courses.forEach(c => {
-        document.getElementById('courses').innerHTML +=
-            "<tr><td>" + c.id + "</td><td>" + c.name + "</td><td>" + c.organizer + "</td><td>" + c.trainer + "</td></tr>";});
+        document.getElementById('courses').innerHTML = "";
+        "<tr><td>" + c.id + "</td><td>" + c.name + "</td><td>" + c.organizer + "</td><td>" + c.trainer + "</td><td><button onclick='removeCourse()'>Törlés</button></td></tr>";});
 }
 
 function setTabledata() {
@@ -131,4 +134,78 @@ function createOwner() {
             console.error('Error:', error);
         });
 
+}
+function createCourse() {
+    let cname = document.getElementById("cname").value;
+    let organizer = document.getElementById("organizer").value;
+    let trainer = document.getElementById("trainer");
+    
+
+    fetch('http://localhost:25294/course', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(
+            {
+                name: cname, organizer: organizer, trainer: trainer
+            }),
+    })
+        .then(response => response)
+        .then(data => {
+            console.log('Success:', data);
+            getCourses();
+        })
+        .catch((error) => {
+            console.error('Error:', error);
+        });
+
+}
+function removeDog(id) {
+    fetch('http://localhost:25294/dog/'+id, {
+        method: 'DELETE',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: null
+    })
+        .then(response => response)
+        .then(data => {
+            console.log('Success:', data);
+        })
+        .catch((error) => {
+            console.error('Error:', error);
+        });
+}
+function removeOwner(id) {
+    fetch('http://localhost:25294/owner/' + id, {
+        method: 'DELETE',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: null
+    })
+        .then(response => response)
+        .then(data => {
+            console.log('Success:', data);
+        })
+        .catch((error) => {
+            console.error('Error:', error);
+        });
+}
+function removeCourse(id) {
+    fetch('http://localhost:25294/course/' + id, {
+        method: 'DELETE',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: null
+    })
+        .then(response => response)
+        .then(data => {
+            console.log('Success:', data);
+        })
+        .catch((error) => {
+            console.error('Error:', error);
+        });
 }
